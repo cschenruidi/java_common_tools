42dadaa197ff1323c9995d6c590c649f96729363
chenruidi <cschenruidi@163.com>
2019-12-13 16:40:21 +0800

    first commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,5 @@
 import org.omg.Messaging.SyncScopeHelper;
+import sun.management.snmp.jvmmib.JVM_MANAGEMENT_MIB;
 
 public class Test {
     public static void main(String[] args) {
@@ -42,6 +43,67 @@ public class Test {
         String s13 = "hello.";
         System.out.println(s12 == s13);//true
 
+        /**
+         * String操作
+         * 长度：采用的是utf-16的编码
+         * 一个是代码单元一个是代码点
+         */
+        String s14 = "he？";
+        /**
+         *代码单元：
+         * utf - 16：0041
+         * utf - 8 ：41
+         */
 
+        System.out.println(s14.length());
+        //代码点个数
+
+        System.out.println(s14.codePointCount(0,s14.length()));
+        /**
+         * String在拼接中会出现大量的对象
+         * Buffer安全是通过syn锁 ，buffer和bulider都是继承抽象的bulider
+         * 底层都是通过char数组（9）之前，9之后是byte数组
+         * StringBuffer和StringBulider默认初始的容量是16
+         * 能评估先评估字符串的大小（setLength）避免扩容
+         * 扩容是，删除原来数组，创建新的数组，arraycopy
+         */
+        StringBuffer sb = new StringBuffer();
+        sb.append("hello");
+        sb.append("world");
+        sb.append("!");
+        System.out.println(sb);//helloworld!
+        System.out.println(sb.toString()); //helloworld!
+
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append("hello");
+        sb1.append("world");
+        sb1.append("!");
+        System.out.println(sb1);//helloworld!
+        System.out.println(sb1.toString());//hellowprld!
+
+        System.out.println(sb1.length()); //11
+        sb1.setLength(sb1.length()-1);
+        System.out.println(sb1.toString());
+
+        /**
+         * jvm的智能优化
+         */
+        String s15 = "hello" + "," + "world";
+         String s16 = "hello,world";
+         System.out.println(s15 == s16);//优化后在同一位置true
+
+        String s17 = "hello";
+        String s18 = s17 + "world";
+
+        StringBuilder sb3 = new StringBuilder();
+        sb3.append(s17);
+        sb.append("world");
+
+        System.out.println(sb3.toString() == s18);//优化后不在同一个位置
+
+        
+
+         
+                
     }
 }
