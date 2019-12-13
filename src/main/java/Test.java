ecd11c824ad2640f7a33da01fc4211e5e96cca04
chenruidi <cschenruidi@163.com>
2019-12-13 18:56:54 +0800

    second

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,3 @@
-import org.omg.Messaging.SyncScopeHelper;
-import sun.management.snmp.jvmmib.JVM_MANAGEMENT_MIB;
-
 public class Test {
     public static void main(String[] args) {
 
@@ -58,7 +55,7 @@ public class Test {
         System.out.println(s14.length());
         //代码点个数
 
-        System.out.println(s14.codePointCount(0,s14.length()));
+        System.out.println(s14.codePointCount(0, s14.length()));
         /**
          * String在拼接中会出现大量的对象
          * Buffer安全是通过syn锁 ，buffer和bulider都是继承抽象的bulider
@@ -82,17 +79,17 @@ public class Test {
         System.out.println(sb1.toString());//hellowprld!
 
         System.out.println(sb1.length()); //11
-        sb1.setLength(sb1.length()-1);
+        sb1.setLength(sb1.length() - 1);
         System.out.println(sb1.toString());//helloworld
-        sb1.setLength(sb1.length()+10);
+        sb1.setLength(sb1.length() + 10);
         System.out.println(sb1);//helloworld
 
         /**
          * jvm的智能优化
          */
         String s15 = "hello" + "," + "world";
-         String s16 = "hello,world";
-         System.out.println(s15 == s16);//优化后在同一位置true
+        String s16 = "hello,world";
+        System.out.println(s15 == s16);//优化后在同一位置true
 
         String s17 = "hello";
         String s18 = s17 + "world";
@@ -106,17 +103,58 @@ public class Test {
         /**
          * 最快的
          */
-        long time  = System.currentTimeMillis();
+        long time = System.currentTimeMillis();
         StringBuilder sb4 = new StringBuilder();
-        for(int i = 0 ; i < 100000 ; i++){
+        for (int i = 0; i < 100000; i++) {
             sb4.append(i);
         }
-        System.out.println(System.currentTimeMillis()-time);
+        System.out.println(System.currentTimeMillis() - time);
 
 
-        
+        /*
+        去掉空格不使用trim方法
+         */
+        String s19 = " hdihsi sdjsod ";
+        String s20 = s19.trim();
+        System.out.println(s20);//hdihsi sdjsod只能去掉首尾空格
 
-         
-                
+        String s21 = " helo shidhs hdis hi ";
+        String s22 = s21.replace(" ", "");
+        System.out.println(s22);//heloshidhshdishi去掉首尾和中间的空格
+
+        System.out.println(trimAll(s22));
+
+        /*
+        字符串反转
+        格式规范的快捷键ctrl+alt+l
+         */
+        String s23 = "--123 321 ,0 ";
+        System.out.println(reverse(s23));
+        StringBuilder sb6 = new StringBuilder();
+        sb6.append(s23);
+        System.out.println(sb6.reverse().toString());
+
+    }
+
+    private static String reverse(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] array = s.toCharArray();
+        for (int i = array.length-1 ; i >= 0; i--){
+          sb.append(array[i]);
+        }
+        return sb.toString();
+    }
+
+    private static String trimAll(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            if (temp == ' ') {
+                continue;
+            } else {
+                sb.append(temp);
+            }
+        }
+        return sb.toString();
     }
 }
